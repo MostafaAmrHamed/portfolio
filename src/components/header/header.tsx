16422f870b10d574da2e021181f7073e73f85bc4
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-08-14 05:45:09 +0200

    added global css to app

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import styles from './header.module.css';
+
 export const Header: React.FC = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1 className={styles.header}>Header</h1>
     </div>
   );
 };
