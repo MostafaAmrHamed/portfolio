f9fcf2823637bc1fc4f8b7c7f9507d2971dadaa7
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-08-15 16:34:20 +0200

    npm(header): removed boostrap

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Image } from 'react-bootstrap';
 
 import styles from './header.module.css';
-
 import bg from './bg.png';
 import profile from './profile.png';
 
 export const Header: React.FC = () => {
   return (
     <div className={styles.header}>
-      <Image src={bg} height={'450'} width={'100%'} fluid />
-      <Image className={styles.profile} src={profile} height={300} width={300} />
+      <img src={bg} height={'auto'} width={'100%'} alt="Cover" />
+      <img className={styles.profile} src={profile} height={300} width={300} alt="porfilePicture" />
     </div>
   );
 };
