e3191966cb1b07fce1a10c659393734db2d6e0d8
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-08-15 03:07:16 +0200

    added image header and profile picture with respionsive css

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 import styles from './header.module.css';
 
+import bg from './bg.png';
+import profile from './profile.png';
+
 export const Header: React.FC = () => {
   return (
-    <div>
-      <h1 className={styles.header}>Header</h1>
+    <div className={styles.header}>
+      <Image src={bg} height={'450'} width={'100%'} fluid />
+      <Image className={styles.profile} src={profile} height={300} width={300} />
     </div>
   );
 };
