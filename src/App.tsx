033bc91c9f03103e782052b9a5529af7f3b5aa7b
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-08-15 14:25:05 +0200

    feat(navbar): created navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-
+import React from 'react';
+import { Navbar } from './components';
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <Navbar />
     </div>
   );
 }
