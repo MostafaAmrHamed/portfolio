0eeef0bff9c0d29932855b896f4baf783281524c
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-09-11 01:20:02 +0200

    feat(contact): styled input text field and textarea

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -5,7 +5,23 @@ import styles from './contact.module.css';
 export const Contact = () => {
   return (
     <div className={styles.contact}>
-      <h1>Contact</h1>
+      <h1 className={styles.title}>Contact</h1>
+      <div className={styles.container}>
+        <div className={styles.input__container}>
+          <p className={styles.label}>Email</p>
+          <input type="text" className={styles.email} />
+
+          <div className={styles.textarea2}>
+            <div>
+              <p className={styles.label}>Message</p>
+              <textarea className={styles.message}>Write your message..</textarea>
+            </div>
+            <div>
+              <button className={styles.btn}>Send Message</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
