301125b234b1079abb521a7639e42870490dd7bc
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-09-11 21:40:02 +0200

    feat(navbar): set position fixed navabr

--- src/components/navbar/DesktopNavbar.tsx
+++ src/components/navbar/DesktopNavbar.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './navbar.module.css';
 
 export const DesktopNavbar = () => {
   const data = ['About', 'Work', 'Skills', 'Contact', 'Resume'];
+  const [navbar, setNavbar] = useState(false);
+
+  const fixedNavbar = () => {
+    if (window.scrollY >= 750) setNavbar(true);
+    else setNavbar(false);
+  };
+  window.addEventListener('scroll', fixedNavbar);
+
   return (
-    <div className={styles.dekstop__navbar}>
+    <div className={navbar ? styles.dekstop__navbar + ' ' + styles.fixed : styles.dekstop__navbar}>
       <ul className={styles.desktop__links__container}>
         {data.map((item) => {
           return (
