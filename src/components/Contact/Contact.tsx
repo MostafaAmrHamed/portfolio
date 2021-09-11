16142d14496c43506cb40c7e77edf24c6dbf41cb
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-09-11 16:39:47 +0200

    feat(contact): added send message api

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,26 +1,50 @@
+import emailjs from 'emailjs-com';
 import React from 'react';
 
 import styles from './contact.module.css';
+
+const sendEmail = (e: any) => {
+  e.preventDefault();
+
+  emailjs
+    .sendForm('service_ylpvus8', 'template_xzfrcal', e.target, 'user_dat081ElRtKpBVaf1gRRv')
+    .then(
+      (result) => {
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      },
+    );
+  e.target.reset();
+};
 
 export const Contact = () => {
   return (
     <div className={styles.contact}>
       <h1 className={styles.title}>Contact</h1>
       <div className={styles.container}>
-        <div className={styles.input__container}>
-          <p className={styles.label}>Email</p>
-          <input type="text" className={styles.email} />
+        <form onSubmit={sendEmail}>
+          <div className={styles.input__container}>
+            <p className={styles.label}>Email</p>
+            <input type="email" className={styles.email} name="email" required />
 
-          <div className={styles.textarea2}>
-            <div>
-              <p className={styles.label}>Message</p>
-              <textarea className={styles.message} placeholder="Write your message.."></textarea>
-            </div>
-            <div className={styles.send__message}>
-              <button className={styles.btn}>Send message</button>
+            <div className={styles.textarea2}>
+              <div>
+                <p className={styles.label}>Message</p>
+                <textarea
+                  className={styles.message}
+                  placeholder="Write your message.."
+                  name="message"
+                  required
+                ></textarea>
+              </div>
+              <div className={styles.send__message}>
+                <input type="submit" className={styles.btn} value="Send message"></input>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
