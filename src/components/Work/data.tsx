6efe5c057944cd4f5d7683304a9c94d1a3ad53e6
MostafaAmrHamed <mostafaamro5619@gmail.com>
2021-09-12 20:48:37 +0200

    feat(work): added projects for data file

--- src/components/Work/data.tsx
+++ src/components/Work/data.tsx
@@ -1,4 +1,11 @@
 import image from './images/img.jpg';
+import freeTime from './images/FreeTime.jpg';
+import wuzzufClone from './images/WuzzufClone.jpg';
+import landingPage from './images/landingPage.png';
+import pigGame from './images/Pig Game.png';
+import montage1 from './images/Montage 1.png';
+import montage2 from './images/Montage 2.jpg';
+import montage3 from './images/Montage 3.jpg';
 type Data = {
   tag: string;
   img: string;
@@ -11,89 +18,90 @@ type Data = {
 export const data: Data = [
   {
     tag: 'web',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: freeTime,
+    description: `Helps you to find a movie to watch made with React, Bootstrap, and JavaScript.`,
     buttons: [
       {
         name: 'View',
-        link: 'https://www.facebook.com/chetos.amr/',
+        link: 'https://mostafaamrhamed.github.io/FreeTime/',
       },
       {
         name: 'Source',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        link: 'https://github.com/MostafaAmrHamed/FreeTime',
       },
     ],
   },
   {
     tag: 'web',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: wuzzufClone,
+    description: `Helps you in your online job search to find jobs made with React, Bootstrap, and JavaScript.`,
     buttons: [
       {
-        name: 'View',
-        link: 'https://www.facebook.com/chetos.amr/',
-      },
-      {
         name: 'Source',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        link: 'https://github.com/MostafaAmrHamed/WuzzufClone',
       },
     ],
   },
   {
     tag: 'web',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: landingPage,
+    description: `Landing page for a company website made with HTML, CSS, Bootstrap, and JavaScript.`,
     buttons: [
       {
         name: 'View',
-        link: 'https://www.facebook.com/chetos.amr/',
+        link: 'https://mostafaamrhamed.github.io/A-company-website/',
       },
       {
         name: 'Source',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        link: 'https://github.com/MostafaAmrHamed/A-company-website',
       },
     ],
   },
   {
     tag: 'web',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: pigGame,
+    description: `A game that teaches probability and is a fun boredom buster for kids made with HTML, CSS and JavaScript.`,
     buttons: [
       {
         name: 'View',
-        link: 'https://www.facebook.com/chetos.amr/',
+        link: 'https://mostafaamrhamed.github.io/Pig-Game/',
       },
       {
         name: 'Source',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        link: 'https://github.com/MostafaAmrHamed/Pig-Game',
       },
     ],
   },
   {
     tag: 'video',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: montage1,
+    description: `A Valorant montage for Marshal, A pro player for team Occupy made using Adobe Premiere  and Photoshop`,
     buttons: [
       {
-        name: 'View',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        name: 'Watch',
+        link: 'https://youtu.be/lP3HPpSb7So',
       },
     ],
   },
   {
     tag: 'video',
-    img: image,
-    description: `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt eligendi dolorum
-          obcaecati fugiat tempore dolor`,
+    img: montage2,
+    description: `A Valorant montage for Marshal made using Adobe Premiere  and Photoshop`,
     buttons: [
       {
-        name: 'View',
-        link: 'https://www.youtube.com/channel/UCt92XPjckv4HiW6ORFFHOxw',
+        name: 'Watch',
+        link: 'https://youtu.be/SKqhJ0AMD2Y',
+      },
+    ],
+  },
+  {
+    tag: 'video',
+    img: montage3,
+    description: `A Valorant montage for Marshal made using Adobe Premiere  and Photoshop`,
+    buttons: [
+      {
+        name: 'Watch',
+        link: 'https://youtu.be/UeZjL0wuF7U',
       },
     ],
   },
